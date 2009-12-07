b2d9d76cba2582884ed9b2fc8ee0166521ef8895
Broader.ZHONG <broader@WarmSun200910.(none)>
2009-12-07 22:07:32 +0800

    Update time:2009-12-07T22:07:32.709791

--- develop/config.py
+++ develop/config.py
@@ -1,13 +1,39 @@
-import yaml,os
+import os
+import yaml
 
-INICONFIG ='config.yaml'
+INICONFIG = os.sep.join((rootdir, 'config.yaml'))
+INIDATA = {}
+INIDATA['userBaseInfo'] = \
+{
+	'fields':( 'prefix', 'firstname', 'lastname', 'gender', \
+				  'organization', 'phone', 'fax', 'aperson', \
+				  'address', 'city', 'country', 'postcode'),
+	'labels':{ 'prefix': _("Prefix :"), 'firstname': _("First Name :"),\
+  				  'lastname': _("Last Name :"), 'gender': _("Gender :"),\
+  				  'organization': _("Organization :"),\
+  				  'phone' : _("Phone :"), 'fax': _("Fax :"),\ 
+  				  'aperson': _("Accompany Person :"),\				    
+  				  'address': _("Address :"),  
+  				  'city': _("City :"), 'country': _("Country :"),\
+  				  'postcode': _("Postal Code :")\
+				}
+}
 
-testdict = {'infoFields':('firstname','lastname','organization'),'nestdict':{'name':'mockname','email':'mockmail'}}
-stream = open(INICONFIG, 'wb')
-yaml.dump(testdict, stream)
-#stream.flush()
-stream.close()
-stream = open(INICONFIG, 'rb')
-config = yaml.load(stream)
-stream.close()
-request=this.cwd
+def _init(fieldsName):	
+	stream = open(INICONFIG, 'wb')
+	yaml.dump(INIDATA,stream,explicit_start=True)
+	stream.close()
+	return	
+
+def _getConfig():
+	stream = open(INICONFIG, 'rb')
+	config = yaml.load(stream)
+	stream.close()
+	return config
+	
+def getData(fieldsName):
+	config = _getConfig()
+	if not config or not config.get(fieldsName):
+		_init(fieldsName)
+	return _getConfig()
+	
