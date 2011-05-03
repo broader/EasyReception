ed330b168a8759a92b9b295518d1f56730040c67
broader <broader.zhong@yahoo.com.cn>
2011-05-03 21:15:38 +0800

    Update time:2011-05-03T21:15:37.116742

--- develop/Karrigell/www/uniwisdom/js/init.js
+++ develop/Karrigell/www/uniwisdom/js/init.js
@@ -1,27 +1,41 @@
-// the global variables
-var spinner4page;
 
-window.addEvent('load', function(){
-    spinner4page = new Spinner('content',{message:"Please wait a minute..."});
-    $('content').set('load', {
-	evalScripts: true,
-	onComplete: function(){
-	    spinner4page.hide(); 
+// add sub menus to clicked main menu item
+function addSubMenu(event){
+    new Event(event).stop();
+    var alink = $(event.target);
+    setActive(alink);
+};
+
+// load page to 'content' container	
+function loadPage(event){
+    new Event(event).stop();
+    
+    // set content spinner
+    spinner4content.show();
+
+    var alink = $(event.target);
+    setActive(alink);
+    var href = alink.getProperty('ref');
+    if(href) $('content').load(href);
+		
+};
+		
+// toggle 'active' css class for clicked menu
+function setActive(alink){
+    $$('#menu ul li').map(function(li){
+	if(li.hasChild(alink) && !li.hasClass('active')){
+	    li.addClass('active');
 	}
+	else if(li.hasClass('active')){
+	    li.removeClass('active');
+	};
     });
+};
 
-    // get menu data in json format	
-    var req = new Request.JSON({
-	url:'config.json', 
-	async:false, 
-	onComplete:renderMenu
-    });
-    req.get();
-});
-	
-function renderMenu(conf){
-    // constructs mianmenu and submenu
-    var lis = conf["menus"].map(function(navMenu,index){
+
+// constructs mianmenu and submenu
+function iniMenu(menus){
+    var lis = menus.map(function(navMenu,index){
 	var alink = new Element('a',{html:navMenu.label, href:'javascript:;'});
 	alink.store('level',0);
 	if($type(navMenu.submenu)==$type('')){
@@ -49,38 +63,90 @@ function renderMenu(conf){
     var ul = new Element('ul');
     $('menu').grab(ul.adopt(lis), 'top');
 };
-		
-// add sub menus to clicked main menu item
-function addSubMenu(event){
-    new Event(event).stop();
-    var alink = $(event.target);
-    setActive(alink);
-};
-
-
-// load page to 'content' container	
-function loadPage(event){
-    new Event(event).stop();
+	
+function renderContent(conf){
     
-    // set content spinner
-    spinner4page.show();
+    document.title = conf['title'][lang];
+    $('logo').getChildren('p')[0].set('text', conf['header'][lang]);
+    
+    iniMenu(conf['menus'][lang]);
 
-    var alink = $(event.target);
-    setActive(alink);
-    var href = alink.getProperty('ref');
-    if(href) $('content').load(href);
-		
-};
-		
-// toggle 'active' css class for clicked menu
-function setActive(alink){
-    $$('#menu ul li').map(function(li){
-	if(li.hasChild(alink) && !li.hasClass('active')){
-	    li.addClass('active');
-	}
-	else if(li.hasClass('active')){
-	    li.removeClass('active');
+    // set language flags
+    $('languageOption').getChildren('a').each(function(item){
+	var href = $(item).getProperty('href');
+	if(href.split('=')[1] == lang){
+	    $(item).addClass('activeLanguage');
 	};
     });
+
+    // set the spinner for 'sidebar' element
+    spinner4sidebar.show();
+    $('sidebar').load(conf['sidebar'][lang]);   
 };
 
+var lang;
+function setLanguage(){
+    // if user has clicked the language flag on the top right corner of the home page,
+    // then use user selected language.
+    lang = location.href.toURI().getData('lang');
+    if(!lang)
+	lang = (MooTools.lang.getCurrentLanguage().toLowerCase().contains('en','-')) ? 'en':'cn';
+
+};
+	
+function languageJudge(){
+    var currentLanguage = navigator.language || navigator.browserLanguage || navigator.userLanguage || navigator.systemLanguage;
+    if(currentLanguage){
+	MooTools.lang.addLanguage(currentLanguage);
+	MooTools.lang.setLanguage(currentLanguage);
+    }
+    
+    setLanguage();
+};	
+
+// the global variables
+var spinner4content, spinner4sidebar; 
+
+window.addEvent('load', function(){
+   
+    languageJudge();
+
+    /*
+    spinner4content = new Spinner('content');
+    spinner4sidebar = new Spinner('sidebar');
+
+    $('content').set('load', {
+	evalScripts: true,
+	onComplete: function(){
+	    spinner4content.hide(); 
+	}
+    });
+
+    // set content spinner
+    spinner4content.show();
+
+    if(href) $('content').load(href);
+    */
+
+    $H(['content','sidebar'].associate(['spinner4content', 'spinner4sidebar']))
+    .each(function(value,key){
+	window[key] = new Spinner(value);
+	$(value).set('load', {
+	    evalScripts: true,
+	    onComplete: function(){
+		window[key].hide(); 
+	    }
+	});
+	
+    });
+     
+    
+
+    // get menu data in json format	
+    var req = new Request.JSON({
+	url:'config.json', 
+	async:false, 
+	onComplete: renderContent
+    });
+    req.get();
+});
